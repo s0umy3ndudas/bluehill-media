1623be26b794d7e2cec6d2d6908c4f0e5c044e53
cyberrrnaut <cyberrrnaut@gmail.com>
2025-03-25 21:20:09 +0530

    added google tag manager

--- bluehills-web/src/app/layout.tsx
+++ bluehills-web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ChatWidget from "@/components/ChatWidget";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,11 +51,47 @@ export default function RootLayout({
           data-pid="TPVZhouzV0HIRMLD"
           data-version="062024"
         />
+
+
+ {/* Google Tag Manager - Head Script */}
+ <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TKZ6M3J5');
+            `,
+          }}
+        />
+
+
+        
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+
+         {/* Google Tag Manager - Body (NoScript) */}
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKZ6M3J5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+
+
         {children}
+
+
+        
         <ChatWidget/>
       </body>
     </html>
