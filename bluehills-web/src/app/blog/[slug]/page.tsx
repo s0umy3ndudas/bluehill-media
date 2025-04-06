2a99021fa86ba3d329c11c724ef0d75df061d467
cyberrrnaut <cyberrrnaut@gmail.com>
2025-04-06 14:53:04 +0530

    added author name

--- bluehills-web/src/app/blog/[slug]/page.tsx
+++ bluehills-web/src/app/blog/[slug]/page.tsx
@@ -147,7 +147,7 @@ export default async function PostPage({
   });
 
   // Extract author if available
-  const author = post.author?.name || "Anonymous";
+  const author = post.author?.name || "Som Das";
   
   // Extract categories if available
   const categories = post.categories?.map((cat: any) => cat.title).join(", ") || "Uncategorized";
@@ -209,14 +209,17 @@ export default async function PostPage({
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
-              <span>{author}</span>
+              <Link href="https://soumyendudas.com" className="underline" target="_blank" rel="noopener noreferrer">
+  <span>{author}</span>
+</Link>
+
             </div>
-            <div className="flex items-center">
+            {/* <div className="flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
               </svg>
               <span>{categories}</span>
-            </div>
+            </div> */}
           </div>
         </div>
       </header>
